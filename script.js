8109186fc9cf41b81077cb23ce86330bded25363
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2021-09-29 16:43:40 +0530

    feat: toCheck

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const buttonCheck = document.querySelector("#check-button");
 const outputBox = document.querySelector("#output-box");
 
 
+function toCheck()
+{
+    let angleOneValue = angleOne.value;
+    let angleTwoValue = angleTwo.value;
+    let angleThreeValue = angleThree.value;
 
+    if(angleOneValue === "" || angleTwoValue === "" || angleThreeValue === "")
+    outputBox.innerText = "Please fill all the fields...";
+    else if(angleOneValue + angleTwoValue + angleThreeValue === 180)
+    outputBox.innerText = "Yes,These angles can form a triangle!!";
+    else
+    outputBox.innerText = "oops,These angles can't form a triangle!!"
+}
+buttonCheck.addEventListener("click", toCheck);
