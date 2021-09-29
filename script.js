82004a6a5b70a397bd4972f69d480e4bfea0d252
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2021-09-29 16:43:13 +0530

    Declared some variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const angleOne = document.querySelector("#angle-one");
+const angleTwo = document.querySelector("#angle-two");
+const angleThree = document.querySelector("#angle-three");
+const buttonCheck = document.querySelector("#check-button");
+const outputBox = document.querySelector("#output-box");
+
+
+
